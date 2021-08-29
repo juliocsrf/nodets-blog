0785be43d3c352c674309af08627837111384fbc
Julio Cesar <juliocsrmf@gmail.com>
2021-08-29 20:55:11 -0300

    Article Model

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -1,19 +1,23 @@
 import { Model, DataTypes } from 'sequelize';
 import connection from '../instances/mysql';
 
-interface CategoryInstance extends Model {
+interface ArticleInstance extends Model {
     title: string;
     slug: string;
+    body: string;
 }
 
-export const Category = connection.define<CategoryInstance>('Category', {
+export const Article = connection.define<ArticleInstance>('Article', {
     title: {
         type: DataTypes.STRING
     },
     slug: {
         type: DataTypes.STRING
+    },
+    body: {
+        type: DataTypes.STRING
     }
 }, {
-    tableName: 'categories',
+    tableName: 'articles',
     timestamps: false
 });
