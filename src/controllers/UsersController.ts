e23f07bbb43f84ad0efabb676f10b3b6a14bfc07
Julio Cesar <juliocsrmf@gmail.com>
2021-09-17 21:25:27 -0300

    Login View and Store

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,16 +1,31 @@
 import { Request, Response } from 'express';
+import { UserService } from '../services/UserService';
+
+let userService = new UserService();
 
 export const index = async (req: Request, res: Response) => {
-    res.render('admin/users/index');
+    let users = await userService.getAll();
+    res.render('admin/users/index', { users });
 }
 
 export const create = async (req: Request, res: Response) => {
-    res.render('admin/users/create');
+    let error = (req.query.error === 'true');
+    res.render('admin/users/create', { error });
 }
 
 export const store = async (req: Request, res: Response) => {
     let email: string = req.body.email;
     let password: string = req.body.password;
+    let name: string = req.body.name;
 
-    res.json({ email, password });
+    if (email && password && name) {
+        let user = await userService.create(name, email, password);
+
+        if (!user) {
+            res.redirect('/admin/users/new?error=true');
+            return;
+        }
+    }
+
+    res.redirect('/admin/users');
 }
